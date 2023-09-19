ed9ea882f6c393c5bdb61b595f040f776ddc86d1
oguzhan <oguzhanngoley@gmail.com>
2023-09-19 10:51:16 +0300

    global error handler added

--- src/main/java/com/springboot/blog/payload/PostDto.java
+++ src/main/java/com/springboot/blog/payload/PostDto.java
@@ -5,12 +5,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Set;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class PostDto {
     private Long id;
+
     private String title;
     private String description;
     private String content;
+    private Set<CommentDto> comments;
 }
